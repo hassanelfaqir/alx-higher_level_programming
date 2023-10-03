76c03361f7b7a7d55a04a834c31cbf3df36f1aca
hassanelfaqir <hassanelfaqir@Linux.myguest.virtualbox.org>
2023-10-03 13:13:17 +0200

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number_s = str(number)
-number_i=int(number_s[-1])
-if number_i > 5:
-    print(f"Last digit of {number} is {number_i} and is greater than 5")
-elif number_i < 6 and number_i != 0:
-    print(f"Last digit of {number} is {number_i} and is less than 6 and not 0")
+if number < 0:
+    number_s = str(number)
+    number_i=int(number_s[-1]) * -1
+    if number_i > 5:
+        print(f"Last digit of {number} is {number_i} and is greater than 5")
+    elif number_i < 6 and number_i != 0:
+        print(f"Last digit of {number} is {number_i} and is less than 6 and not 0 ")
+    else:
+        print(f"Last digit of {number} is {number_i} and is 0")
 else:
-    print(f"Last digit of {number} is {number_i} and is 0")
+    number_s = str(number)
+    number_i = int(number_s[-1])
+    if number_i > 5:
+        print(f"Last digit of {number} is {number_i} and is greater than 5")
+    elif number_i < 6 and number_i != 0:
+        print(f"Last digit of {number} is {number_i} and is less than 6 and not 0 ")
+    else:
+        print(f"Last digit of {number} is {number_i} and is 0")
