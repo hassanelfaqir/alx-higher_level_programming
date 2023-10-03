17c26abe57fb6d7cd8a7c6e04ca2cd2daff3241d
hassanelfaqir <hassanelfaqir@Linux.myguest.virtualbox.org>
2023-10-03 15:11:58 +0200

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    if chr(char) is not 'q' and chr(char) is not 'e':
-        print("{}".format(chr(char)), end="")
+for lt in range(97, 123):
+    if chr(lt) is not 'q' and chr(lt) is not 'e':
+        print("{}".format(chr(lt)), end="")
