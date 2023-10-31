b7441a55e161fea5959cdf55c1724e93a3e523a7
hassanelfaqir <hassanelfaqir@Linux.myguest.virtualbox.org>
2023-10-31 11:49:22 +0100

    0x09-python-everything_is_objec

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called .
+    """
+    __slots__ = ["first_name"]
